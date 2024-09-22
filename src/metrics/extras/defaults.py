f7d0c838f9a74dce44cdda545690fc987d38286f
Nirbhay Gupta <nirbhay.gupta@junio.in>
2024-09-22 12:01:59 +0530

    release: v1.2.4

--- src/metrics/extras/defaults.py
+++ src/metrics/extras/defaults.py
@@ -2,7 +2,7 @@ from .shared import Metric
 
 
 defaults = [
-    Metric("counter", "http_requests_total", "Counts all the http requests", labels=["method", "endpoint", "status", "version"]),
-    Metric("gauge", "http_requests_active", "Tracks all the active http requests", labels=["method", "endpoint", "version"]),
-    Metric("histogram", "http_requests_latency", "Counts all http requests latency", labels=["method", "endpoint", "status", "version"])
+    Metric("counter", "http_requests_total", "Counts all the http requests", labels=["method", "path", "status", "version"]),
+    Metric("gauge", "http_requests_active", "Tracks all the active http requests", labels=["method", "path", "version"]),
+    Metric("histogram", "http_requests_latency", "Counts all http requests latency", labels=["method", "path", "status", "version"])
 ]
